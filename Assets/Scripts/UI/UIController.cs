db09218e9502b824c363ab421d7c93c2b16da2ca
Priscila da Rocha Alves <priscila.mizuiky@gmail.com>
2024-04-30 14:51:43 -0300

    adicionado parte do controller de dialogo + atualizacoes na hierarquia + adicao de place holders

---
+++ Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,71 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField]
+    private DialogBox _dialogBox;
+
+    public void Init()
+    {
+        CtrlGameManager.Instance.DialogController.onStartDialog += OpenDialogBox;
+        CtrlGameManager.Instance.DialogController.onEndDialog += CloseDialogBox;
+        CtrlGameManager.Instance.DialogController.onUpdateOptions += SetDialogOptions;
+        CtrlGameManager.Instance.DialogController.onEnableNextButton += HideOptions;
+        CtrlGameManager.Instance.DialogController.onChangePortrait += UpdatePortrait;
+
+        DialogOption.onChooseOption += SelectOption;
+
+        _dialogBox.Init();
+    }
+
+    public void Reset()
+    {
+        _dialogBox.ResetFields();
+    }
+
+    #region DialogBox
+
+    public void OpenDialogBox()
+    {
+        _dialogBox.SetBoxVisibility(true);
+    }
+
+    public void CloseDialogBox()
+    {
+        _dialogBox.SetBoxVisibility(false);
+    }
+
+    public void SetDialogOptions(List<Option> options)
+    {
+        _dialogBox.SetOptions(options);
+    }
+
+    public void HideOptions()
+    {
+        _dialogBox.HideDialogOptions();
+    }
+
+    public void UpdatePortrait(Sprite newPortrait)
+    {
+        _dialogBox.ChangePortrait(newPortrait);
+    }
+
+    public void SelectOption(int option)
+    {
+        Debug.Log($"next dialog index: {option}");
+        CtrlGameManager.Instance.DialogController.ChangeToNextDialog(option);
+    }
+    #endregion
+
+    private void OnDisable()
+    {
+        CtrlGameManager.Instance.DialogController.onStartDialog -= OpenDialogBox;
+        CtrlGameManager.Instance.DialogController.onEndDialog -= CloseDialogBox;
+        CtrlGameManager.Instance.DialogController.onUpdateOptions -= SetDialogOptions;
+        CtrlGameManager.Instance.DialogController.onEnableNextButton -= HideOptions;
+        CtrlGameManager.Instance.DialogController.onChangePortrait -= UpdatePortrait;
+
+        DialogOption.onChooseOption -= SelectOption;
+    }
+}
