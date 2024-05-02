320f2e0fa3cf731db7fec49f4ea92492221b3c9d
Priscila da Rocha Alves <priscila.mizuiky@gmail.com>
2024-05-02 11:00:56 -0300

    fixed wrong dialog panel position

--- Assets/Scripts/Dialog/DialogBox.cs
+++ Assets/Scripts/Dialog/DialogBox.cs
@@ -1,9 +1,7 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
-using UnityEngine.EventSystems;
 
 public class DialogBox : MonoBehaviour
 {
